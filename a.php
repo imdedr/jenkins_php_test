7b2e47e0855107850048fb5f5ffa3120a30f4548
imdedr <imdedr@gmail.com>
2015-11-18 23:11:51 +0800

    phpunit test

---
+++ a.php
@@ -0,0 +1,14 @@
+<?php
+
+function f1() {
+	echo 'something...'.'<br>';
+}
+
+function f2() {
+	echo 'something...'.'<br>';
+}
+
+f1();
+f2();
+echo 'something...'.'<br>';
+echo 'something...'.'<br>';
